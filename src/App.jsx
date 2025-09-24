9a6254ef17302f0bd6ef98ce2a6e1bce66b83479
johnsame1 <129014486+johnsame1@users.noreply.github.com>
2025-09-24 13:59:32 +0300

    lastedits

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/NavBar';
-import Home from './Components/Pages/Home';
+import Home from './Pages/Home';
 import { useEffect } from 'react';
 import ReactPixel from 'react-facebook-pixel';
 
 function App() {
   useEffect(() => {
     // هنا بتحط الـ Pixel ID بتاعك
-    ReactPixel.init("687884864344366");  
+    ReactPixel.init('687884864344366');
     ReactPixel.pageView(); // يسجل أول PageView
   }, []);
 
   return (
     <>
       <Router>
-        <Navbar/>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
